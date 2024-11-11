f6894dfc677dc49e4bdae0eab180a155826e2478
WallScripts <wallcejosiel3@gmail.com>
2024-11-10 21:08:06 -0300

    Create WallHubTest.lua

---
+++ WallHubTest.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/WallScripts/OrionLibraryV3/refs/heads/main/WallHub")))()
+local Window = OrionLib:MakeWindow({IntroText = "Wall Hub(testes)", IntroIcon = "rbxassetid://8834748103",Name = ("Wall Hub(testes)".." | ".. identifyexecutor()),IntroToggleIcon = "rbxassetid://7734091286", HidePremium = false, SaveConfig = false, InEnabled = true, ConfigFolder = "Wall Hub"})
+
+local Tab = Window:MakeTab({
+	Name = "Testes",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
