f2bcc1c14591a7b787a22a53fb6c88e8a2f3b55c
WallScripts <wallcejosiel3@gmail.com>
2024-11-12 12:49:01 -0300

    Update WallHubTest.lua

--- WallHubTest.lua
+++ WallHubTest.lua
@@ -8,67 +8,27 @@ local infoTab = Window:MakeTab({
 })
 
 
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
--- Função para atualizar o dropdown com os nomes dos jogadores
-local function UpdatePlayerList()
-    local playerNames = {}  -- Tabela para armazenar os nomes dos jogadores
-    for _, p in pairs(Players:GetPlayers()) do
-        table.insert(playerNames, p.Name)  -- Adiciona o nome do jogador à tabela
-    end
-
-    -- Atualiza o dropdown com a lista de jogadores
-    infoTab:UpdateDropdown({
-        Name = "Jogadores",
-        Options = playerNames,
-        Callback = function(value)
-            -- Aqui você pode usar a função de copiar quando um jogador for selecionado
-            copyToClipboard(value)  -- Copia o nome do jogador selecionado
-        end
-    })
-end
-
--- Cria a aba de "Informações Extra"
-local infoTab = OrionLib:MakeTab({
-    Name = "Informações Extra",
-    Icon = "rbxassetid://7733911828",
-    PremiumOnly = false
-})
-
--- Exibe as informações do jogador e do jogo
-infoTab:AddLabel("Nome do Player: " .. player.Name)
-infoTab:AddLabel("ID do Player: " .. player.UserId)
-infoTab:AddLabel("Nome do Game: " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
-infoTab:AddLabel("ID do Game: " .. game.PlaceId)
-
--- Adiciona o botão para copiar o nome do jogador
-infoTab:AddButton({
-    Name = "Copiar Nome do Player",
-    Callback = function()
-        copyToClipboard(player.Name)
-    end
-})
-
--- Adiciona o botão para copiar o ID do jogador
-infoTab:AddButton({
-    Name = "Copiar ID do Player",
-    Callback = function()
-        copyToClipboard(tostring(player.UserId))
-    end
-})
-
--- Cria o dropdown de jogadores
-infoTab:AddDropdown({
-    Name = "Selecionar Jogador",
-    Options = {},  -- Inicialmente vazio
+local SpeedSlider = Tab:AddSlider({
+    Name = "WalkSpeed",
+    Min = 0,
+    Max = 400,
+    Default = 16,
+    Color = Color3.fromRGB(0, 255, 0),
+    Increment = 1,
+    ValueName = "",
     Callback = function(value)
-        -- Copia o nome do jogador selecionado
-        copyToClipboard(value)
-    end
+        local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:WaitForChild("Humanoid")
+        humanoid.WalkSpeed = value        
+    end    
 })
 
--- Atualiza a lista de jogadores no dropdown
-UpdatePlayerList()
+Tab:AddButton({
+	Name = "Resetar Velocidade",
+	Callback = function()SpeedSlider:Set(16)
+      		print("Velocidade resetada")
+  	end    
+})
 
 OrionLib:Init()
