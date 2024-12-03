d5c45cd88882acee2a7617a694629209e6df6ad4
Wallace <wallcejosiel3@gmail.com>
2024-12-02 23:41:34 -0300

    Update WallHubTest.lua

--- WallHubTest.lua
+++ WallHubTest.lua
@@ -2,36 +2,12 @@ local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Wal
 local Window = OrionLib:MakeWindow({IntroText = "Wall Hub(testes)", IntroIcon = "rbxassetid://8834748103",Name = ("Wall Hub(testes)".." | ".. identifyexecutor()),IntroToggleIcon = "rbxassetid://7734091286", HidePremium = false, SaveConfig = false, InEnabled = true, ConfigFolder = "Wall Hub"})
 
 local TestTab = Window:MakeTab({ Name = "Testes", Icon = "rbxassetid://4483345998", })
-local hasInteracted = false -- Variável para rastrear a interação do jogador
 
-TestTab:AddToggle({
-    Name = "Noclip",
-    Default = false, -- Noclip desativado por padrão
-    Callback = function(value)
-        if not hasInteracted then
-            hasInteracted = true -- Marca que o jogador interagiu
-        end
+local function Notification(ScriptName) 
+      OrionLib:MakeNotification({ Name = "Wall Hub System", Content = "O " .. ScriptName .. " foi executado", Image = "rbxassetid://".. Id, Time = 5 })
+end
 
-        Clip = not value
+TestTab:AddButton({
+    Name = "Nome do Botão", Callback = function(); Notification("teste, 4483345998") end })
 
-        if Clip then
-            if Noclipping then
-                Noclipping:Disconnect()
-            end
-            for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-                if child:IsA("BasePart") then
-                    child.CanCollide = true
-                end
-            end
-
-            -- Evita notificação "Off" na primeira inicialização
-            if hasInteracted then
-                NotificationOff("Noclip")
-            end
-        else
-            Noclipping = game:GetService("RunService").Stepped:Connect(NoclipLoop)
-            NotificationOn("Noclip")
-        end
-    end
-})
 OrionLib:Init()
